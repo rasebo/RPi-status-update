217b0d41b0715d1a48f3fc36c20a79ff337c1621
RaSeBo <razvan.botez@gmail.com>
2012-11-09 21:23:32 +0200

    Added more info to the sent stats

--- RPi-status-update.sh
+++ RPi-status-update.sh
@@ -3,14 +3,7 @@
 #emails are being sent to an address with both offline logs and new system info
 #modify the vars below with your data (log file, stats file, email, binary paths)
 
-#files, email address, ip check address
-#script_location="$HOME/.rpiupdate/external_ip.sh"
-log_file_offline="$HOME/.rpiupdate/RPi-offline.log"
-stats_file="$HOME/.rpiupdate/rpi-stats-file"
-email_addr="email@domain.com"
-icmp_check_addr="google.com"
-ip_scan_range="192.168.1.0/24"
-#variables for binary locations
+#binary locations
 wget="/usr/bin/wget"
 echo="/bin/echo"
 mail="/usr/bin/mail"
@@ -19,6 +12,20 @@ egrep_cmd="/bin/egrep"
 date="/bin/date"
 sleep_cmd="/bin/sleep"
 nmap="/usr/bin/nmap"
+hostname="/bin/hostname"
+who="/usr/bin/w"
+cat="/bin/cat"
+#files, email address, ip check address
+#script_location="$HOME/.rpiupdate/external_ip.sh"
+log_file_offline="$HOME/.rpiupdate/RPi-offline.log"
+stats_file="$HOME/.rpiupdate/rpi-stats-file"
+email_addr="email@domain.com"
+icmp_check_addr="google.com"
+ip_scan_range="192.168.1.0/24"
+external_ip=`$wget -q -t 5 --output-document=- "http://automation.whatismyip.com/n09230945.asp"` > /dev/null 2>&1
+internal_ip=`$hostname -I`
+memory_total=`$cat /proc/meminfo | awk ' /MemTotal/ { print $2 } '`
+memory_free=`$cat /proc/meminfo | awk ' /MemFree/ { print $2 } '`
 #icmp status check command
 status=`$ping -c 1 $icmp_check_addr 2>&1 | $egrep_cmd -c "\<unknown\>|\<unreachable\>"`
 # Search for .rpiupdate folder and create it together with log file and stat file
@@ -37,18 +44,29 @@ until [ $status -eq 0 ]; do
     $sleep_cmd 30
 done
 fi
-#create the file whoose contents will be emailed to $email_addr
-external_ip=`$wget -q -t 5 --output-document=- "http://automation.whatismyip.com/n09230945.asp"` > /dev/null 2>&1
+#build the file for emailing to $email_addr
 #$echo -n "" > "$stats_file"
 $echo "Un fleac, m-au restartat..." > "$stats_file"
-$echo "" >> "$stats_file"
-$echo EXTERNAL IP: "$external_ip" >> "$stats_file"
-$echo HOSTNAME: `/bin/hostname` >> "$stats_file"
-$echo KERNEL: `/bin/uname -s -v -r -m` >> "$stats_file"
-$echo HOSTS UP ARE: >> "$stats_file"
+$echo . >> "$stats_file"
+$echo External IP: "$external_ip" >> "$stats_file"
+$echo . >> "$stats_file"
+$echo Internal IP: "$internal_ip" >> "$stats_file"
+$echo . >> "$stats_file"
+$echo Hostname: `$hostname` >> "$stats_file"
+$echo . >> "$stats_file"
+$echo Kernel info: `/bin/uname -s -v -r -m` >> "$stats_file"
+$echo . >> "$stats_file"
+$echo Total Memory: "$memory_total" kB >> "$stats_file"
+$echo . >> "$stats_file"
+$echo Free Memory: "$memory_free" kB >> "$stats_file"
+$echo . >> "$stats_file"
+$echo Who: >> "$stats_file"
+$who >> "$stats_file" 
+$echo . >> "$stats_file"
+$echo -n Local network hosts: >> "$stats_file"
 $nmap -sP $ip_scan_range >> "$stats_file"  
 #mail logs and stat info
-$mail -s "RPi - New IP information" $email_addr < "$stats_file"
+$mail -s "RPi - Status" $email_addr < "$stats_file"
 $mail -s "RPi - Offline logs" $email_addr < "$log_file_offline" > /dev/null 2>&1 #don't need the output, redirect to /dev/null is for when the logfile is empty 
 #done
 exit 0
