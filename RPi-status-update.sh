44069c9af6b3f2da78d63f0dcc75fc3b9b86fab6
RaSeBo <razvan.botez@gmail.com>
2012-11-10 09:38:25 +0200

    now only sends one email

--- RPi-status-update.sh
+++ RPi-status-update.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# modify script/create new script that checks for new ip every 10 mins and sends email if change is detected
+
 #simple script for checking your RPi's external IP address 
 #emails are being sent to an address with both offline logs and new system info
 #modify the vars below with your data (log file, stats file, email, binary paths)
@@ -16,25 +19,24 @@ hostname="/bin/hostname"
 who="/usr/bin/w"
 cat="/bin/cat"
 #files, email address, ip check address
-#script_location="$HOME/.rpiupdate/external_ip.sh"
-log_file_offline="$HOME/.rpiupdate/rpi-offline.log"
-stats_file="$HOME/.rpiupdate/rpi-stats-file"
+install_dir="$HOME/.rpiupdate"
+stats_file="$install_dir/rpi-stats-file"
 email_addr="email@domain.com"
 icmp_check_addr="google.com"
 ip_scan_range="192.168.1.0/24"
 memory_total=`$cat /proc/meminfo | awk ' /MemTotal/ { print $2 } '`
 memory_free=`$cat /proc/meminfo | awk ' /MemFree/ { print $2 } '`
-#icmp status check command
-status=`$ping -c 1 $icmp_check_addr 2>&1 | $egrep_cmd -c "\<unknown\>|\<unreachable\>"`
 # Search for .rpiupdate folder and create it together with log file and stat file
 if [ ! -d $HOME/.rpiupdate/ ]; then
     mkdir $HOME/.rpiupdate/
     touch $log_file_offline
     touch $stats_file
 fi
-#if there is no icmp reply, add separator to log file and until there's icmp reply, add a log entry every 30 seconds
+#define $status ICMP check command
+status=`$ping -c 1 $icmp_check_addr 2>&1 | $egrep_cmd -c "\<unknown\>|\<unreachable\>"`
+#if there is no ICMP reply, create a new logfile and add a log entry every 30 seconds untill we have ICMP reply
 if [ $status -eq 1 ]; then
-    echo "===========================" >> $log_file_offline 
+    log_file_offline="$install_dir/rpi-offline-`date +%s`"
 until [ $status -eq 0 ]; do
     status=`$ping -c 1 $icmp_check_addr 2>&1 | $egrep_cmd -c "\<unknown\>|\<unreachable\>"`
     $echo -n "Offline - " >> $log_file_offline
@@ -42,32 +44,36 @@ until [ $status -eq 0 ]; do
     $sleep_cmd 30
 done
 fi
-#define internal and external ip vars
+#define internal & external ip vars now - if defined earlier, ip check won't work if the host has no initial inet connectivity 
 external_ip=`$wget -q -t 5 --output-document=- "http://automation.whatismyip.com/n09230945.asp"` > /dev/null 2>&1
 internal_ip=`$hostname -I`
 #build the file for emailing to $email_addr
-#$echo -n "" > "$stats_file"
 $echo "Un fleac, m-au restartat..." > "$stats_file"
-$echo . >> "$stats_file"
+$echo  >> "$stats_file"
 $echo External IP: "$external_ip" >> "$stats_file"
-$echo . >> "$stats_file"
+$echo  >> "$stats_file"
 $echo Internal IP: "$internal_ip" >> "$stats_file"
-$echo . >> "$stats_file"
+$echo  >> "$stats_file"
 $echo Hostname: `$hostname` >> "$stats_file"
-$echo . >> "$stats_file"
+$echo  >> "$stats_file"
 $echo Kernel info: `/bin/uname -s -v -r -m` >> "$stats_file"
-$echo . >> "$stats_file"
+$echo  >> "$stats_file"
 $echo Total Memory: "$memory_total" kB >> "$stats_file"
-$echo . >> "$stats_file"
+$echo  >> "$stats_file"
 $echo Free Memory: "$memory_free" kB >> "$stats_file"
-$echo . >> "$stats_file"
+$echo  >> "$stats_file"
 $echo Who: >> "$stats_file"
 $who >> "$stats_file" 
-$echo . >> "$stats_file"
+$echo  >> "$stats_file"
 $echo -n Local network hosts: >> "$stats_file"
-$nmap -sP $ip_scan_range >> "$stats_file"  
+$nmap -sP $ip_scan_range >> "$stats_file" 
+#check if we have an offline log file and if true, add it to the mail
+if [ -a $log_file_offline  ];then
+   $echo  >> "$stats_file"
+   $echo I have been disconnected from the Internet: >> "$stats_file" 
+   $echo "$log_file_offline" >> "$stats_file"
+fi 
 #mail logs and stat info
 $mail -s "RPi - Status" $email_addr < "$stats_file"
-$mail -s "RPi - Offline logs" $email_addr < "$log_file_offline" > /dev/null 2>&1 #don't need the output, redirect to /dev/null is for when the logfile is empty 
 #done
 exit 0
